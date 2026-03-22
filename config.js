f1789248ad284c8982116d35f39901a71cc540b5
lpsosonline <lpsosnotify@gmail.com>
2026-03-22 21:32:27 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// ⚠ NEVER commit this file to GitHub / Git
+// Add "config.js" to your .gitignore file
+
+const SUPABASE_URL = "https://qwcngezsbtlorkvgyeqz.supabase.co";
+const SUPABASE_KEY = "sb_publishable_as7dj3l-qsSVEf3FrZS97A_DtuE4tj5";
